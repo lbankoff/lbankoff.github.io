45bb68778651330c5b8cb88dbfaff77136e4f5e6
lbankoff <35219385+lbankoff@users.noreply.github.com>
2018-01-08 12:48:22 +0300

    Site files

---
+++ some_scipts_v2.js
@@ -0,0 +1,180 @@
+var price = document.getElementById("Price Value");
+  
+function GetPrice(seat, row)
+{
+	if(row == 01)
+	{
+		if(seat >= 01 && seat <= 24)
+			document.getElementById("Price Value").innerHTML = "5 рупий будьте любезны";
+		else if(seat >= 25 && seat <= 32)
+			document.getElementById("Price Value").innerHTML = "10 рупий будьте любезны";
+		else
+			document.getElementById("Price Value").innerHTML = "Выберите ряд и место, чтобы узнать цену";
+	}
+	if(row == 02)
+	{
+		if(seat >= 01 && seat <= 24)
+			document.getElementById("Price Value").innerHTML = "5 рупий будьте любезны";
+		else if(seat >= 25 && seat <= 32)
+			document.getElementById("Price Value").innerHTML = "10 рупий будьте любезны";
+		else
+			document.getElementById("Price Value").innerHTML = "Выберите ряд и место, чтобы узнать цену";
+	}
+	if(row == 03)
+	{
+		if(seat >= 01 && seat <= 24)
+			document.getElementById("Price Value").innerHTML = "5 рупий будьте любезны";
+		else if(seat >= 25 && seat <= 32)
+			document.getElementById("Price Value").innerHTML = "10 рупий будьте любезны";
+		else
+			document.getElementById("Price Value").innerHTML = "Выберите ряд и место, чтобы узнать цену";
+	}
+	if(row == 04)
+	{
+		if(seat >= 01 && seat <= 24)
+			document.getElementById("Price Value").innerHTML = "5 рупий будьте любезны";
+		else if(seat >= 25 && seat <= 32)
+			document.getElementById("Price Value").innerHTML = "10 рупий будьте любезны";
+		else
+			document.getElementById("Price Value").innerHTML = "Выберите ряд и место, чтобы узнать цену";
+	}
+	if(row == 05)
+	{
+		if(seat >= 01 && seat <= 24)
+			document.getElementById("Price Value").innerHTML = "5 рупий будьте любезны";
+		else if(seat >= 25 && seat <= 32)
+			document.getElementById("Price Value").innerHTML = "10 рупий будьте любезны";
+		else
+			document.getElementById("Price Value").innerHTML = "Выберите ряд и место, чтобы узнать цену";
+	}
+	if(row == 06)
+	{
+		if(seat >= 01 && seat <= 24)
+			document.getElementById("Price Value").innerHTML = "5 рупий будьте любезны";
+		else if(seat >= 25 && seat <= 32)
+			document.getElementById("Price Value").innerHTML = "10 рупий будьте любезны";
+		else
+			document.getElementById("Price Value").innerHTML = "Выберите ряд и место, чтобы узнать цену";
+	}
+	if(row == 07)
+	{
+		if(seat >= 01 && seat <= 24)
+			document.getElementById("Price Value").innerHTML = "5 рупий будьте любезны";
+		else if(seat >= 25 && seat <= 32)
+			document.getElementById("Price Value").innerHTML = "10 рупий будьте любезны";
+		else
+			document.getElementById("Price Value").innerHTML = "Выберите ряд и место, чтобы узнать цену";
+	}
+	if(row == 08)
+	{
+		if(seat >= 01 && seat <= 24)
+			document.getElementById("Price Value").innerHTML = "5 рупий будьте любезны";
+		else if(seat >= 25 && seat <= 32)
+			document.getElementById("Price Value").innerHTML = "10 рупий будьте любезны";
+		else
+			document.getElementById("Price Value").innerHTML = "Выберите ряд и место, чтобы узнать цену";
+	}
+	if(row == 09)
+	{
+		if(seat >= 01 && seat <= 24)
+			document.getElementById("Price Value").innerHTML = "5 рупий будьте любезны";
+		else if(seat >= 25 && seat <= 32)
+			document.getElementById("Price Value").innerHTML = "10 рупий будьте любезны";
+		else
+			document.getElementById("Price Value").innerHTML = "Выберите ряд и место, чтобы узнать цену";
+	}
+	if(row == 10)
+	{
+		if(seat >= 01 && seat <= 24)
+			document.getElementById("Price Value").innerHTML = "5 рупий будьте любезны";
+		else if(seat >= 25 && seat <= 32)
+			document.getElementById("Price Value").innerHTML = "10 рупий будьте любезны";
+		else
+			document.getElementById("Price Value").innerHTML = "Выберите ряд и место, чтобы узнать цену";
+	}
+	if(row == 11)
+	{
+		if(seat >= 01 && seat <= 24)
+			document.getElementById("Price Value").innerHTML = "5 рупий будьте любезны";
+		else if(seat >= 25 && seat <= 32)
+			document.getElementById("Price Value").innerHTML = "10 рупий будьте любезны";
+		else
+			document.getElementById("Price Value").innerHTML = "Выберите ряд и место, чтобы узнать цену";
+	}
+	if(row == 12)
+	{
+		if(seat >= 01 && seat <= 24)
+			document.getElementById("Price Value").innerHTML = "5 рупий будьте любезны";
+		else if(seat >= 25 && seat <= 32)
+			document.getElementById("Price Value").innerHTML = "10 рупий будьте любезны";
+		else
+			document.getElementById("Price Value").innerHTML = "Выберите ряд и место, чтобы узнать цену";
+	}
+	if(row == 13)
+	{
+		if(seat >= 01 && seat <= 24)
+			document.getElementById("Price Value").innerHTML = "5 рупий будьте любезны";
+		else if(seat >= 25 && seat <= 32)
+			document.getElementById("Price Value").innerHTML = "10 рупий будьте любезны";
+		else
+			document.getElementById("Price Value").innerHTML = "Выберите ряд и место, чтобы узнать цену";
+	}
+	if(row == 14)
+	{
+		if(seat >= 01 && seat <= 24)
+			document.getElementById("Price Value").innerHTML = "5 рупий будьте любезны";
+		else if(seat >= 25 && seat <= 32)
+			document.getElementById("Price Value").innerHTML = "10 рупий будьте любезны";
+		else
+			document.getElementById("Price Value").innerHTML = "Выберите ряд и место, чтобы узнать цену";
+	}
+	if(row == 15)
+	{
+		if(seat >= 01 && seat <= 24)
+			document.getElementById("Price Value").innerHTML = "5 рупий будьте любезны";
+		else if(seat >= 25 && seat <= 32)
+			document.getElementById("Price Value").innerHTML = "10 рупий будьте любезны";
+		else
+			document.getElementById("Price Value").innerHTML = "Выберите ряд и место, чтобы узнать цену";
+	}
+	if(row == 16)
+	{
+		if(seat >= 01 && seat <= 24)
+			document.getElementById("Price Value").innerHTML = "5 рупий будьте любезны";
+		else if(seat >= 25 && seat <= 32)
+			document.getElementById("Price Value").innerHTML = "10 рупий будьте любезны";
+		else
+			document.getElementById("Price Value").innerHTML = "Выберите ряд и место, чтобы узнать цену";
+	}
+	if(row == 17)
+	{
+		if(seat >= 01 && seat <= 24)
+			document.getElementById("Price Value").innerHTML = "5 рупий будьте любезны";
+		else if(seat >= 25 && seat <= 32)
+			document.getElementById("Price Value").innerHTML = "10 рупий будьте любезны";
+		else
+			document.getElementById("Price Value").innerHTML = "Выберите ряд и место, чтобы узнать цену";
+	}
+	if(row == 18)
+	{
+		if(seat >= 01 && seat <= 24)
+			document.getElementById("Price Value").innerHTML = "5 рупий будьте любезны";
+		else if(seat >= 25 && seat <= 32)
+			document.getElementById("Price Value").innerHTML = "10 рупий будьте любезны";
+		else
+			document.getElementById("Price Value").innerHTML = "Выберите ряд и место, чтобы узнать цену";
+	}
+	if(row == 19)
+	{
+		if(seat >= 01 && seat <= 24)
+			document.getElementById("Price Value").innerHTML = "5 рупий будьте любезны";
+		else if(seat >= 25 && seat <= 32)
+			document.getElementById("Price Value").innerHTML = "10 рупий будьте любезны";
+		else
+			document.getElementById("Price Value").innerHTML = "Выберите ряд и место, чтобы узнать цену";
+	}
+	
+	
+	//alert("Seat: " + value + "\nRow: " + name);
+	//document.getElementById("Price Value").innerHTML = "25 рупий будьте любезны";
+}
